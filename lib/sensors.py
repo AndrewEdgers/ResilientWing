73a409aaf63f5fd20b9253e25f646769332d0fca
Edgers <a.b.23042000@gmail.com>
2023-10-30 12:42:30 +0200

    updated sensors.py

--- lib/sensors.py
+++ lib/sensors.py
@@ -15,66 +15,80 @@ limitations under the License.
 """
 
 import smbus
+import math
 import time
 
-# Initialize the SMBus for I2C communication
-bus = smbus.SMBus(1)
 
-# Constants (Replace these with your sensor's actual I2C address and registers)
-GYRO_I2C_ADDRESS = 0x68
-COMPASS_I2C_ADDRESS = 0x1E
+class MPU6050:
+    # MPU-6050 Registers
+    POWER_MGMT_1 = 0x6b
+    GYRO_XOUT = 0x43
+    GYRO_YOUT = 0x45
+    GYRO_ZOUT = 0x47
+    ACCEL_XOUT = 0x3b
+    ACCEL_YOUT = 0x3d
+    ACCEL_ZOUT = 0x3f
 
+    def __init__(self, address=0x68, bus_num=1):
+        self.bus = smbus.SMBus(bus_num)
+        self.address = address
+        # Wake up sensor
+        self.bus.write_byte_data(self.address, self.POWER_MGMT_1, 0)
 
-def initialize_gyro():
-    # Initialization sequence for the gyroscope
-    # Write to specific registers to set up the gyroscope
-    pass
+    def _read_word(self, adr):
+        high = self.bus.read_byte_data(self.address, adr)
+        low = self.bus.read_byte_data(self.address, adr + 1)
+        val = (high << 8) + low
+        return val
 
+    def _read_word_2c(self, adr):
+        val = self._read_word(adr)
+        if val >= 0x8000:
+            return -(65536 - val)
+        return val
 
-def initialize_compass():
-    # Initialization sequence for the compass
-    # Write to specific registers to set up the compass
-    pass
+    def _get_rotation(self, x, y, z):
+        radians_x = math.atan2(y, self._dist(x, z))
+        radians_y = math.atan2(x, self._dist(y, z))
+        return {
+            'x': math.degrees(radians_x),
+            'y': -math.degrees(radians_y)
+        }
 
+    @staticmethod
+    def _dist(a, b):
+        return math.sqrt(a * a + b * b)
 
-def read_gyro():
-    # Read raw gyroscope data from the sensor
-    # Convert it to angular velocity (degrees/sec or radians/sec)
-    # Return the angular velocity
-    pass
+    def get_gyro_data(self):
+        x = self._read_word_2c(self.GYRO_XOUT) / 131
+        y = self._read_word_2c(self.GYRO_YOUT) / 131
+        z = self._read_word_2c(self.GYRO_ZOUT) / 131
+        return {'x': x, 'y': y, 'z': z}
 
+    def get_accel_data(self):
+        x = self._read_word_2c(self.ACCEL_XOUT) / 16384.0
+        y = self._read_word_2c(self.ACCEL_YOUT) / 16384.0
+        z = self._read_word_2c(self.ACCEL_ZOUT) / 16384.0
+        return {'x': x, 'y': y, 'z': z}
 
-def read_compass():
-    # Read raw magnetometer data from the sensor
-    # Convert it to magnetic field strength or orientation
-    # Return the magnetic field data
-    pass
-
-
-def calibrate_gyro():
-    # Optional: Perform calibration to remove gyroscope bias
-    pass
-
-
-def calibrate_compass():
-    # Optional: Perform calibration to improve compass accuracy
-    pass
-
-
-def perform_dead_reckoning(angular_velocity, delta_time):
-    # Perform dead reckoning based on gyroscope data
-    # Update and return the estimated orientation of the UAV
-    pass
+    def get_rotation_angles(self):
+        accel_data = self.get_accel_data()
+        return self._get_rotation(accel_data['x'], accel_data['y'], accel_data['z'])
 
 
 if __name__ == "__main__":
-    initialize_gyro()
-    initialize_compass()
+    sensor = MPU6050()
 
     while True:
-        angular_velocity = read_gyro()
-        compass_data = read_compass()
+        gyro_data = sensor.get_gyro_data()
+        accel_data = sensor.get_accel_data()
+        rotation = sensor.get_rotation_angles()
 
-        # Perform dead reckoning or other calculations here
+        print("Gyroscope Data (°/s): X: {:.3f}, Y: {:.3f}, Z: {:.3f}".format(gyro_data['x'], gyro_data['y'],
+                                                                             gyro_data['z']))
+        print("Accelerometer Data (g): X: {:.3f}, Y: {:.3f}, Z: {:.3f}".format(accel_data['x'], accel_data['y'],
+                                                                               accel_data['z']))
+        print("Rotation Angles (°): X: {:.3f}, Y: {:.3f}".format(rotation['x'], rotation['y']))
+        print("-------------------------------------------------------")
 
-        time.sleep(0.1)  # sleep for 100ms
+        time.sleep(1)
